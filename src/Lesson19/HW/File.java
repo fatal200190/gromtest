662b4ddd24896e0ad4b4aa266386784eecc5f453
fatal200190 <hlushko.olexandr@gmail.com>
2017-11-13 20:14:58 +0200

    word

---
+++ src/Lesson19/HW/File.java
@@ -0,0 +1,45 @@
+package Lesson19.HW;
+
+/**
+ * Created by Alex on 06.11.2017.
+ */
+public class File {
+    private long id;
+    private String name;
+    private String format;
+    private long size;
+
+    public File(long id, String name, String format, long size) {
+        this.id = id;
+        this.name = name;
+        this.format = format;
+        this.size = size;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", format='" + format + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
+
