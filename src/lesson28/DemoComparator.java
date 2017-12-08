42cd54815fcebd754ed9914f072aad58a91484ea
fatal200190 <hlushko.olexandr@gmail.com>
2017-12-08 21:15:19 +0200

    ...

--- src/lesson28/DemoComparator.java
+++ src/lesson28/DemoComparator.java
@@ -6,22 +6,31 @@ import java.util.Date;
 
 public class DemoComparator {
     public static void main(String[] args) {
-        Capability capability1 = new Capability(1001,"test","rrrr",true, new Date());
-        Capability capability2 = new Capability(1005,"test","rrrr",false, new Date());
-        Capability capability3 = new Capability(900,"test","rrrr",true, new Date());
-        Capability capability4 = new Capability(900,"test","rrrr",false, new Date());
+        Capability capability1 = new Capability(1001,"test4","rrrr",true, new Date());
+        Capability capability2 = new Capability(1005,null,"rrrr3",false, new Date());
+        Capability capability3 = new Capability(1004,"test1",null,true, new Date());
+        Capability capability4 = new Capability(1002,"test2","rrrr2",false, null);
+        Capability capability5 = new Capability(1006,"test3","abc",false, new Date());
+        Capability capability6 = new Capability(1006,"test3","bcd",false, new Date());
 
         ArrayList<Capability>capabilities = new ArrayList<>();
         capabilities.add(capability1);
         capabilities.add(capability2);
         capabilities.add(capability3);
         capabilities.add(capability4);
+        capabilities.add(capability5);
+        capabilities.add(capability6);
 
+
+//        System.out.println(capabilities);
+//
+//        capabilities.sort(new IsActiveComparator());
+//
+//        System.out.println(capabilities);
+
+        capabilities.sort(new FullComparator());
         System.out.println(capabilities);
 
-        capabilities.sort(new IsActiveComparator());
-
-        System.out.println(capabilities);
 
     }
 }
--- src/lesson28/FullComparator.java
+++ src/lesson28/FullComparator.java
@@ -5,7 +5,16 @@ import java.util.Comparator;
 public class FullComparator implements Comparator<Capability> {
     @Override
     public int compare(Capability o1, Capability o2) {
-        if (!o1.getChannelName().equals(o2.getChannelName()))
+        if ((o1.getChannelName() == null && o2.getChannelName() != null) ||
+                (o1.getFingerprint() == null && o2.getFingerprint() != null) || (o1.getDateCreated() == null && o2.getDateCreated() != null))
+            return 1;
+        if ((o1.getChannelName() != null && o2.getChannelName() == null) ||
+                (o1.getFingerprint() != null && o2.getFingerprint() == null) || (o1.getDateCreated() != null && o2.getDateCreated() == null))
+            return -1;
+        if ((o1.getChannelName() == null && o2.getChannelName() == null) ||
+                (o1.getFingerprint() == null && o2.getFingerprint() == null) || (o1.getDateCreated() == null && o2.getDateCreated() == null))
+            return 0;
+        if (!o1.getChannelName().equals(o2.getFingerprint()))
             return o1.getChannelName().compareTo(o2.getChannelName());
         else if (!o1.getFingerprint().equals(o2.getFingerprint()))
             return o1.getFingerprint().compareTo(o2.getFingerprint());
