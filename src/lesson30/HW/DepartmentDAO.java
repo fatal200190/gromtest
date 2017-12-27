747ad3b145548bde8e698da3fc73253eace5faf3
fatal200190 <hlushko.olexandr@gmail.com>
2017-12-27 13:18:07 +0200

    ...

--- src/lesson30/HW/DepartmentDAO.java
+++ src/lesson30/HW/DepartmentDAO.java
@@ -4,14 +4,5 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DepartmentDAO {
-    EmployeeDAO employeeDAO = new EmployeeDAO();
-    public Set<Employee> findEmployeeByProject(Department department, String project){
-        Set<Employee>result = new HashSet<>();
-        for (Employee employee : department.getEmployees()){
-            if (employee != null && employeeDAO.findProjectFromEmployee(employee,project))
-                result.add(employee);
-        }
-        return result;
-    }
 
 }
--- src/lesson30/HW/EmployeeDAO.java
+++ src/lesson30/HW/EmployeeDAO.java
@@ -1,6 +1,24 @@
 package lesson30.HW;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EmployeeDAO {
+    static Set<Employee> employees = new HashSet<>();
+
+    public Employee saveEmployee(Employee employee){
+        employees.add(employee);
+        return employee;
+    }
+    public Set<Employee> findEmployeeByProject(Department department, String project){
+        Set<Employee>result = new HashSet<>();
+        for (Employee employee : department.getEmployees()){
+            if (employee != null && findProjectFromEmployee(employee,project))
+                result.add(employee);
+        }
+        return result;
+    }
+
     public boolean findProjectFromEmployee(Employee employee, String projectName){
         for (Project project : employee.getProjects()){
             if (project.getName() != null && project.getName().equals(projectName))
